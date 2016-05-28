88175a05e6d2b0a48d32dd36554315df950b3bf7
mireaulf <mireaulf+github2@gmail.com>
2016-05-28 11:33:38 -0400

    chore(package): add test for parse

--- mbuff/package.py
+++ mbuff/package.py
@@ -2,18 +2,20 @@
 import re
 
 class package:
-    regex = re.compile('package [a-zA-Z0-9_]+;')
+    regex = re.compile('package ([a-zA-Z0-9_]+);')
     def __init__(self, stream):
         if stream is None:
             raise ValueError('stream is null')
         if not isinstance(stream, str):
             raise TypeError('stream is not string')
         self.stream = stream;
+        self.name = ''
     def parse(self):
         search_res = self.regex.match(self.stream)
-        parse_res = tuple(False, ())
+        parse_res = (False, None)
         if search_res is not None:
-            parse_res = tuple(True, self.regex.sub('', self.stream))
+            self.name = search_res.group(1)
+            parse_res = (True, self.regex.sub('', self.stream))
         return parse_res
 
 if __name__ == "__main__":
--- mbuff/package_test.py
+++ mbuff/package_test.py
@@ -10,6 +10,27 @@ class package_test(unittest.TestCase):
         stream = 'Hello world!'    
         pkg = package(stream)
         self.assertEqual(stream, pkg.stream)
+    def test_parse(self):
+        stream = 'hello World!'
+        pkg = package(stream)
+        res = pkg.parse()
+        self.assertEqual(False, res[0])
+        self.assertEqual(None, res[1])
+        stream = 'this package first_package;'
+        pkg = package(stream)
+        res = pkg.parse()
+        self.assertEqual(False, res[0])
+        self.assertEqual(None, res[1])
+        stream = ' package first_package; asd '
+        pkg = package(stream)
+        res = pkg.parse()
+        self.assertEqual(False, res[0])
+        self.assertEqual(None, res[1])
+        pkg = package(stream.strip())
+        res = pkg.parse()
+        self.assertEqual(True, res[0])
+        self.assertEqual(' asd', res[1])
+        self.assertEqual('first_package', pkg.name)
 
 if __name__ == '__main__':
     unittest.main()
